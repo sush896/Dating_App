aa997faca8b69921875320f8cb1fd12c087fabc2
Sushmita Manche <msush9689@gmail.com>
2025-01-21 19:55:38 +0530

    End of Section 7

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -9,9 +9,9 @@ namespace API.DTOs
     public class RegisterDto
     {
         [Required]
-        public required string Username { get; set; }
+        public string Username { get; set; } = string.Empty;
 
         [Required]
-        public required string Password { get; set; }
+        public string Password { get; set; } = string.Empty;
     }
 }
