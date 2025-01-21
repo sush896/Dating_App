7d90b6edff0edd9aad808af4382ec83522c16410
Sushmita Manche <msush9689@gmail.com>
2025-01-21 13:35:49 +0530

    End of Section 5

---
+++ client/src/app/_models/user.ts.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace client.src.app._models
+{
+    public class user.ts
+    {
+        
+    }
+}
